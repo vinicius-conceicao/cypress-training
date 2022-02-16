ace5b207d06db227fb8214f86a638f3d7b089e36
Vinicius <vinicius.conceicao9@gmail.com>
2022-02-16 11:56:02 -0300

    updated code

--- cypress/Pages/SignupPage.js
+++ cypress/Pages/SignupPage.js
@@ -9,7 +9,7 @@ class SignupPage {
     }
 
     fillForm(deliver) {
-        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="fullName"]').type(deliver.name)
         cy.get('input[name="cpf"]').type(deliver.cpf)
         cy.get('input[name="email"]').type(deliver.email)
         cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
@@ -40,7 +40,8 @@ class SignupPage {
     }
 
     alertMessageShouldBe(expectedMessage) {
-        cy.get('.alert-error').should('have.text', expectedMessage)
+        //cy.get('.alert-error').should('have.text', expectedMessage)
+        cy.contains('.alert-error', expectedMessage).should('be.visible')
 
 
     }
--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,9 +1,9 @@
 
 
-describe('home page', ()=> {
-    it('app deve estar online', ()=> {
+describe('home page', () => {
+    it('app deve estar online', () => {
         cy.viewport(1440, 900)
-        cy.visit('https://buger-eats.vercel.app')
+        cy.visit('https://buger-eats-qa.vercel.app')
         cy.get('h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
     })
 })
