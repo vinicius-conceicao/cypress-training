41116de4f241a88f77ab078029f3dde32115b28f
Vinicius <vinicius.conceicao9@gmail.com>
2022-02-16 10:17:36 -0300

    first commit

---
+++ cypress/integration/cadastro.spec.js
@@ -0,0 +1,27 @@
+import signup from '../Pages/SignupPage'
+
+describe('Cadastro', () => {
+    beforeEach(function () {
+        cy.fixture('deliver').then((d) => {
+            this.deliver = d
+
+        })
+    })
+
+    it('Usuário deve se tornar um entregador', function () {
+        signup.go()
+        signup.fillForm(this.deliver.signup)
+        signup.submit()
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        signup.modalContentShouldBe(expectedMessage)
+
+    })
+
+    it('CPF incorreto', function () {
+        signup.go()
+        signup.fillForm(this.deliver.cpf_inv)
+        signup.submit()
+        signup.alertMessageShouldBe('Oops! CPF inválido')
+    })
+
+})
